1c765f4d373ccaa1db93676a192c0fffb57bf549
Casey Lee <casey.lee@stelligent.com>
2017-05-05 15:42:10 -0700

    integrate with consul

--- src/main/java/com/stelligent/BananaApplication.java
+++ src/main/java/com/stelligent/BananaApplication.java
@@ -3,10 +3,11 @@ package com.stelligent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EntityScan(
 				basePackageClasses = { BananaApplication.class, Jsr310JpaConverters.class }
 )
