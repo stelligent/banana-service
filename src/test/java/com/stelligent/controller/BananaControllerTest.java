182cfc83762b257137ebf75fb9a6f6f57ef2eb0c
Casey Lee <casey.lee@stelligent.com>
2017-05-11 09:09:41 -0700

    fix unit tests by disabling consul in test

--- src/test/java/com/stelligent/controller/BananaControllerTest.java
+++ src/test/java/com/stelligent/controller/BananaControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -29,6 +30,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BananaController.class)
+@TestPropertySource(locations = {
+        "classpath:application-test.yml"})
 public class BananaControllerTest {
 
   @Autowired
